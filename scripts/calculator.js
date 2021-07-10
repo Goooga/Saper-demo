e723863db1977bda8db9eade5f3bb62665de43af
Goooga <73659422+Goooga@users.noreply.github.com>
2021-07-10 22:12:38 +0300

    323232

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,11 +2,72 @@ const exp = document.getElementById("exp");
 exp.innerText = " ";
 let first_number = 0;
 let second_number = 0;
+let sign = "0";
+let ident = 1;
 function number(i){
+    if (sign == "."){
+        ident =ident * 10;
+        first_number = first_number + i/ident;
+        exp.innerText = first_number.toString();
+    } else {
     first_number = first_number*10+i;
-    exp.innerText = first_number.toString();
+    exp.innerText = first_number.toString();}
 }
-function first(i){
-    first_number = first_number/i;
-    exp.innerText = first_number.toString();
+
+function share(){
+    sign = "/";
+    second_number = first_number;
+    first_number = 0;
+    exp.innerText = sign;
+}
+
+function multiply(){
+    sign = "*";
+    second_number = first_number;
+    first_number = 0;
+    exp.innerText = sign;
+}
+
+function subtract() {
+    sign = "-";
+    second_number = first_number;
+    first_number = 0;
+    exp.innerText = sign;
+}
+
+function add() {
+    sign = "+";
+    second_number = first_number;
+    first_number = 0;
+    exp.innerText = sign;
+}
+
+function dot(){
+    first_number = first_number;
+    sign = ".";
+    exp.innerText = first_number.toString() + sign;
+}
+
+function equally(){
+    if(sign == "/"){
+        second_number = second_number/first_number;
+        exp.innerText = second_number.toString();
+        first_number = 0;
+        ident = 1;
+    } else if (sign == "*"){
+        second_number = second_number * first_number;
+        exp.innerText = second_number.toString();
+        first_number = 0;
+        ident = 1;
+    } else if (sign == "-"){
+        second_number = second_number - first_number;
+        exp.innerText = second_number.toString();
+        first_number = 0;
+        ident = 1;
+    } else if (sign == "+"){
+        second_number = second_number + first_number;
+        exp.innerText = second_number.toString();
+        first_number = 0;
+        ident = 1;
+    }
 }
